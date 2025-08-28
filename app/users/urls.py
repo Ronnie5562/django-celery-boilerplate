7a4a7621a121bad4f3f93ae60ad7a48ca8f2d72b
Ronald Abimbola <abimbolaaderinsola212@gmail.com>
2025-08-28 14:13:54 +0200

    Modified the users app url config

--- app/users/urls.py
+++ app/users/urls.py
@@ -10,13 +10,35 @@ from rest_framework_simplejwt.views import (
 app_name = "users"
 
 urlpatterns = [
-    path("", views.ListCreateUserView.as_view(), name="users_list_create"),
-    path("<uuid:id>/", views.DetailUserView.as_view(), name="user_details"),
-    path("me/", views.ManageProfileView.as_view(), name="manage_profile"),
+    path(
+        "",
+        views.ListCreateUserView.as_view(),
+        name="users_list_create"
+    ),
+    path(
+        "<uuid:id>/",
+        views.DetailUserView.as_view(),
+        name="user_details"
+    ),
+    path(
+        "me/",
+        views.ManageProfileView.as_view(),
+        name="manage_profile"
+    ),
     path(
         "activate/<str:uidb64>/<str:token>/",
         views.ActivateAccountAPIView.as_view(),
-        name="activate_account",
+        name="activate",
+    ),
+    path(
+        'password-reset/',
+        views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password-reset-confirm/<str:uidb64>/<str:token>/',
+        views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
     ),
     path(
         "token/", views.JWTCookieTokenObtainPairView.as_view(),
@@ -27,6 +49,14 @@ urlpatterns = [
         views.JWTCookieTokenRefreshView.as_view(),
         name="token_refresh",
     ),
-    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    path("logout/", views.LogOutAPIView.as_view(), name="token_verify"),
+    path(
+        "token/verify/",
+        TokenVerifyView.as_view(),
+        name="token_verify"
+    ),
+    path(
+        "logout/",
+        views.LogOutAPIView.as_view(),
+        name="token_verify"
+    ),
 ]
